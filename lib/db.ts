c9bd62aea60097398f8e1b00dce3b6b1ab952cd8
scottlee <scottlee0191@gmail.com>
2024-05-07 19:22:55 +0700

    add migrate

--- lib/db.ts
+++ lib/db.ts
@@ -1,29 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import path from "path";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var cachedPrisma: PrismaClient;
-}
-
-// Workaround to find the db file in production
-const filePath = path.join(process.cwd(), "prisma/local.db");
-const config = {
-  datasources: {
-    db: {
-      url: "file:" + filePath,
-    },
-  },
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient(config);
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient(config);
-  }
-  prisma = global.cachedPrisma;
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-export const db = prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
